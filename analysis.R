7a9837b6e629a7453861999db265c50304768b16
theericstone <ericstone@me.com>
2014-05-09 10:04:54 -0400

    commiting some shiiiiiiiiiit

---
+++ analysis.R
@@ -0,0 +1,93 @@
+# this is the file to read the data and transform it
+library(data.table)
+library(ggplot2)
+
+source("util_functions.R")
+murder <- fread("ny_murder_data.csv")
+murder.melt <- MeltDataTable(murder,
+                             variable.name="outcome", value.name="n_cases",
+                             id.vars=c("year","death_penalty_illegal","death_penalty_moratorium","death_status","dispositions_total"))
+murder.melt[ ,p_cases := n_cases/dispositions_total ]
+#overall summary of outcomes
+ggplot(murder.melt, aes(x=year, y=n_cases, group = outcome, fill = outcome)) +
+  geom_area( position = "fill") + theme_bw()
+
+#time series of outcomes
+rect_dat <- data.table( start = 1995, end_1 = 2004, end_2 = 2007 )
+ggplot(murder.melt, aes(x=year, y=n_cases, group = outcome, color = outcome)) +
+  geom_line() + theme_bw() + facet_wrap( ~ outcome, ncol = 3) +
+  geom_rect(data=rect_dat, aes(xmin=start, xmax=end_1), 
+            ymin=0, ymax=15, alpha=0.3, fill="grey60", inherit.aes = FALSE) +
+  geom_rect(data=rect_dat, aes(xmin=end_1, xmax=end_2), 
+            ymin=0, ymax=15, alpha=0.3, fill="grey80", inherit.aes = FALSE) +
+  theme(legend.position = "none")
+
+#only related to murder 1
+ggplot(murder.melt[ outcome %in% c("pled_murder_1","convicted_murder_1") ],
+       aes(x=year, y=p_cases, group = outcome, color = outcome)) +
+#   geom_smooth(se = FALSE, method = "loess", fullrange=TRUE) + ylim(c(0,8.5)) +
+  geom_smooth( se = FALSE, method="loess", size = 1.5 ) + ylim(c(0,1)) +
+  geom_point(size = 2, alpha = .4) +
+  theme_bw() + facet_grid( ~ outcome ) + 
+  geom_rect(data=rect_dat, aes(xmin=start, xmax=end_1), 
+            ymin=0, ymax=15, alpha=0.3, fill="grey60", inherit.aes = FALSE) +
+  geom_rect(data=rect_dat, aes(xmin=end_1, xmax=end_2), 
+            ymin=0, ymax=15, alpha=0.3, fill="grey80", inherit.aes = FALSE) +
+  ggtitle("Percentage of Total Murder Trials") +
+  theme(legend.position = "none")
+
+pleas <- murder.melt[ outcome %in% c("pled_other","pled_murder_1") ]
+pleas[ ,pleas_total := sum(n_cases), by="year" ]
+pleas[ ,p_pleas := n_cases / pleas_total ]
+ggplot(pleas,
+       aes(x=year, y=p_pleas, group = outcome, color = outcome, fill=outcome)) +
+  #   geom_smooth(se = FALSE, method = "loess", fullrange=TRUE) + ylim(c(0,8.5)) +
+  geom_smooth( se = FALSE, method="loess", size = 1.5 ) + ylim(c(0,1)) +
+  geom_point(size = 2, alpha = .4) +
+  theme_bw() + facet_grid( ~ outcome ) + 
+  geom_rect(data=rect_dat, aes(xmin=start, xmax=end_1), 
+            ymin=0, ymax=15, alpha=0.3, fill="grey60", inherit.aes = FALSE) +
+  geom_rect(data=rect_dat, aes(xmin=end_1, xmax=end_2), 
+            ymin=0, ymax=15, alpha=0.3, fill="grey80", inherit.aes = FALSE) +
+  ggtitle("Percentage of Total Pleas Filed") +
+  theme(legend.position = "none")
+
+#only pleas
+pleas[ ,p_all_pleas := pleas_total / dispositions_total ]
+pleas[ year >= 2007 ]$death_status <- "illegal_again"
+ggplot(unique(pleas[,list(year,p_all_pleas,death_status)]),
+       aes(x=year, y=p_all_pleas, color=death_status)) +
+  #   geom_smooth(se = FALSE, method = "loess", fullrange=TRUE) + ylim(c(0,8.5)) +
+  geom_smooth( se = FALSE, method="loess", size = 1.5 ) + ylim(c(0,1)) +
+  geom_point(size = 2, alpha = .4) +
+  theme_bw() + 
+  geom_rect(data=rect_dat, aes(xmin=start, xmax=end_1), 
+            ymin=0, ymax=15, alpha=0.3, fill="grey60", inherit.aes = FALSE) +
+  geom_rect(data=rect_dat, aes(xmin=end_1, xmax=end_2), 
+            ymin=0, ymax=15, alpha=0.3, fill="grey80", inherit.aes = FALSE) +
+  ggtitle("Percentage of Dispositions that Were Pleas") +
+  theme(legend.position = "none")
+
+#for assessing whether outcomes changed as a result of death penalty illegality
+murder.skinny <- rbindlist(
+  apply(murder.melt[ n_cases > 0 ], 1, function(row)
+    data.table(year = row["year"],
+               dp_illegal = row["death_penalty_illegal"],
+               dp_morat = row["death_penalty_moratorium"],
+               dp_status = row["death_status"],
+               outcome = rep(row["outcome"],row["n_cases"]))
+  ))
+murder.skinny$case_id <- 1:nrow(murder.skinny)
+
+ggplot(murder.melt[outcome %in% c("pled_murder_1"),
+                   list( n_cases = sum(n_cases),
+                         p_cases = sum(n_cases) / sum(dispositions_total) ), by="death_status"],
+       aes( x=death_status, y=p_cases)) +
+  geom_bar(stat="identity") + theme_bw() +
+  ylab("Percent of Dispositions that are Murder Pleas")
+
+dp.illegal.lm <- glm(outcome_fac ~ dp_status,
+                     data=murder.skinny[ outcome %in% c("pled_other","pled_murder_1") ][
+                       ,outcome_fac := as.numeric(outcome == "pled_murder_1") ],
+                     family = "binomial")
+summary(dp.illegal.lm)
---
+++ util_functions.R
@@ -0,0 +1,107 @@
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Helpful functions that don't exist in other packages
+## 
+## Creator: Eric Stone (ericstone@me.com) 
+## melt code inspired by this stackoverflow post:
+## http://stackoverflow.com/questions/18449938/pivot-on-data-table-similar-to-rehape-melt-function
+####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#replicates the functionality of hadley's melt but for data tables
+#as of april 2014 this is included in the CRAN data.table release
+MeltDataTable <- function(dt,
+                          variable.name = "variable",
+                          value.name = "value",
+                          measure.vars = NULL,
+                          id.vars = NULL){
+  if(is.null(id.vars))
+    stop("Sorry, you need to specify the 'id.vars' for this melt.")
+  if(is.null(measure.vars))
+    measure.vars <- names(dt)[ which(!names(dt) %in% id.vars) ]
+  
+  dt.melt <- dt[,which(names(dt) %in% c(measure.vars,id.vars)), with=FALSE]
+  
+  by.statment <- paste(id.vars, collapse=",")
+  dt.melt <- dt.melt[ ,list(variable = names(.SD), value = unlist(.SD, use.names = F)),
+                     by = by.statment ]
+  setnames(dt.melt, c("variable","value"), c(variable.name, value.name))
+  return(dt.melt)
+}
+
+#Replace NAs with a user-defined value in a Data Table
+RemoveNas = function(table = dt,
+                      na.to = 0) {
+  for (j in seq_len(ncol(table)))
+    set(table,which(is.na(table[[j]])),j,na.to)
+}
+
+#measures of vectors
+Skewness <- function(x){
+  xx <- x[!is.na(x)]
+  n <- length(xx)
+  d <- xx - mean(xx)
+  sqrt(n) * sum(d^3)/(sum(d^2)^(3/2))
+}
+
+Kurtosis <- function(x){
+  xx <- x[!is.na(x)]
+  n <- length(xx)
+  d <- xx - mean(xx)
+  r <- n * sum(d^4)/(sum(d^2)^2)
+  r-3
+}
+
+Bimodality <- function(x){
+  x <- x[!is.na(x)]
+  n <- length(x)
+  (Skewness(x)^2+1) / (Kurtosis(x) + (3*((n-1)^2) / ((n-2)*(n-3)))) 
+}
+
+
+#merge more than two data frames or tables together
+MergeDataFrames <- function(list.of.data.frames,
+                            by=NULL,
+                            all=NULL){
+  if(is.null(by)) stop("You need to explicitly define your 'by' statement")
+  if(is.null(all)) {
+    all <- FALSE
+    message("MergeDataFrames: only complete rows will be returned. Use all=TRUE to return all rows.")
+  }
+  Reduce(
+    function(...) 
+      merge(...,
+            by=by,
+            all=all), 
+    list.of.data.frames)
+}
+
+#function for rounding any number "x" to the nearest "r" (.1, .01, .5, etc.)
+RoundTo <- function(x,r,direction = c("round","ceiling","floor")){ 
+  direction <- match.arg(direction)
+  init <- switch(direction,
+                 round = round(x/r),
+                 ceiling = ceiling(x/r),
+                 floor = floor(x/r))
+  return( init * r )
+}
+  
+#produces expected results for modular arithmetic with non-integer numbers
+Mod <- function(n,m){
+  if( m >= n ) return(n)
+  n.digits <- nchar(strsplit(paste(n),".",fixed=T)[[1]][2])
+  div <- floor(n/m)
+  rem <- n - (div * m)
+  return(round(rem,n.digits))
+}
+
+#produces bootstrapped statistics for a vector
+BootStrap <- function(fcasts, n.rep = 500, statistic = list(mean, var)){
+  if(!is.numeric(fcasts)) stop("You must supply a numeric vector of fcasts of at least length 1")
+  straps <- replicate(n=n.rep, sample(fcasts, replace=TRUE))
+  boots <- rbindlist( apply(straps, 2, function(x)
+    data.table( t(sapply(1: length(statistic), function(op){
+      tmp <- data.table(statistic[[op]](x))
+      setnames(tmp, paste("stat",op,sep="_"))
+      return(tmp)})) ) ) )
+  boot.straps <- apply(boots, 2, as.numeric)
+  return(data.table(boot.straps))
+}
